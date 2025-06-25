03016802164a89c722cbcc2e9a0e7a7c0a0c554f
sagargautam.codehimalaya <sagargautam.codehimalaya@gmail.com>
2025-06-25 10:12:22 +0545

    nav manage

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<div class="container-fluid bg-secondary ps-5 pe-0 d-none d-lg-block">
+<div class="container-fluid bg-secondary ps-5 pe-0">
     <div class="row gx-0">
         <div class="col-md-6 text-center text-lg-start mb-2 mb-lg-0">
             <div class="d-inline-flex align-items-center">
@@ -9,10 +9,10 @@
                 {{-- <a class="text-body py-2 px-3 border-end" href=""><small>Support</small></a>
                 <a class="text-body py-2 px-3 border-end" href=""><small>Privacy</small></a>
                 <a class="text-body py-2 px-3 border-end" href=""><small>Policy</small></a> --}}
-                <a class="text-body py-2 ps-3" href="{{route('career')}}"><small>Career</small></a>
+                <a class="text-body py-2 ps-3" href="{{ route('career') }}"><small>Career</small></a>
             </div>
         </div>
-        <div class="col-md-6 text-center text-lg-end">
+        <div class="col-md-6 d-lg-block d-none text-center text-lg-end">
             <div class="position-relative d-inline-flex align-items-center bg-primary text-white top-shape px-5">
                 <div class="me-3 pe-3 border-end py-2">
                     <p class="m-0"><i class="fa fa-envelope-open me-2"></i>info@example.com</p>
